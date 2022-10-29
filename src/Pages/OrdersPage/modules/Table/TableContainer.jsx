3b5a297f30ec850294b8b5c774910abbc6de7c8e
AIllarionov <hellomyguest@yandex.ru>
2022-10-29 10:48:26 +0300

    TableContainer reworked

--- src/Pages/OrdersPage/modules/Table/TableContainer.jsx
+++ src/Pages/OrdersPage/modules/Table/TableContainer.jsx
@@ -3,42 +3,44 @@ import { FilterContext } from '../../../../store/filterContext';
 import { Table } from './Table';
 import styles from './TableBody/TableBody.module.css';
 
+const ORDER_MAP = {
+  new: {
+    name: 'Новый',
+    iconType: 'Dot',
+    iconClassName: styles.icon_postponed,
+  },
+  calculation: {
+    name: 'Рассчет',
+    iconType: 'Dot',
+    iconClassName: styles.icon_calculation,
+  },
+  confirmed: {
+    name: 'Подтвержден',
+    iconType: 'Dot',
+    iconClassName: styles.icon_completed,
+  },
+  postponed: {
+    name: 'Отложен',
+    iconType: 'Dot',
+    iconClassName: styles.icon_postponed,
+  },
+  completed: {
+    name: 'Выполнен',
+    iconType: 'Checkmark',
+    iconClassName: styles.icon_completed,
+    textClassName: styles.text_completed,
+  },
+  declined: {
+    name: 'Отменен',
+    iconType: 'Abort',
+    iconClassName: styles.icon_canceled,
+    declined: true,
+  },
+};
+
 export function TableContainer() {
   const { orders } = useContext(FilterContext);
-  const ORDER_MAP = {
-    new: {
-      name: 'Новый',
-      iconType: 'Dot',
-      iconClassName: styles.icon_postponed,
-    },
-    calculation: {
-      name: 'Рассчет',
-      iconType: 'Dot',
-      iconClassName: styles.icon_calculation,
-    },
-    confirmed: {
-      name: 'Подтвержден',
-      iconType: 'Dot',
-      iconClassName: styles.icon_completed,
-    },
-    postponed: {
-      name: 'Отложен',
-      iconType: 'Dot',
-      iconClassName: styles.icon_postponed,
-    },
-    completed: {
-      name: 'Выполнен',
-      iconType: 'Checkmark',
-      iconClassName: styles.icon_completed,
-      textClassName: styles.text_completed,
-    },
-    declined: {
-      name: 'Отменен',
-      iconType: 'Abort',
-      iconClassName: styles.icon_canceled,
-      declined: true,
-    },
-  };
+
   const ordersExtended = orders.map((item) => {
     // eslint-disable-next-line no-param-reassign
     item.statusExtended = {
@@ -50,5 +52,6 @@ export function TableContainer() {
     };
     return item;
   });
+
   return <Table orders={ordersExtended} />;
 }
