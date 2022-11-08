68eee0782646d899afd5195b930ec33d45780ede
AIllarionov <hellomyguest@yandex.ru>
2022-11-09 02:03:01 +0300

    filters improved

--- src/store/selectors/getFilteredOrders.js
+++ src/store/selectors/getFilteredOrders.js
@@ -1,3 +1,5 @@
+import { createSelector } from '@reduxjs/toolkit';
+
 const orders = (state) => state.orders.orders;
 const searchValue = (state) => state.filter.searchValue;
 const dateFromValue = (state) => state.filter.dateFromValue;
@@ -6,53 +8,54 @@ const statusValue = (state) => state.filter.statusValue;
 const priceFromValue = (state) => state.filter.priceFromValue;
 const priceToValue = (state) => state.filter.priceToValue;
 
-export const filteredOrders = (state) => {
-  const data = orders(state);
-  const search = searchValue(state);
-  const dateFrom = dateFromValue(state);
-  const dateTo = dateToValue(state);
-  const status = statusValue(state);
-  const priceFrom = priceFromValue(state);
-  const priceTo = priceToValue(state);
-
-  let arr = data.slice(0);
-  if (search !== '') {
-    arr = arr.filter(
-      (item) =>
-        item.orderNumber.indexOf(search) !== -1 ||
-        item.customer.toLowerCase().indexOf(search.toLowerCase()) !== -1
-    );
+export const filteredOrders = createSelector(
+  orders,
+  searchValue,
+  dateFromValue,
+  dateToValue,
+  statusValue,
+  priceFromValue,
+  priceToValue,
+  (data, search, dateFrom, dateTo, status, priceFrom, priceTo) => {
+    let arr = data.slice(0);
+    if (search !== '') {
+      arr = arr.filter(
+        (item) =>
+          item.orderNumber.indexOf(search) !== -1 ||
+          item.customer.toLowerCase().indexOf(search.toLowerCase()) !== -1
+      );
+    }
+    if (dateFrom !== '') {
+      arr = arr.filter((item) => {
+        const [d, m, y] = item.date.slice(0, 10).split('.');
+        const [df, mf, yf] = dateFrom.split('.');
+        const date = Date.parse(`${y}-${m}-${d}`);
+        const datefrom = Date.parse(`${yf}-${mf}-${df}`);
+        return datefrom < date;
+      });
+    }
+    if (dateTo !== '') {
+      arr = arr.filter((item) => {
+        const [d, m, y] = item.date.slice(0, 10).split('.');
+        const [dt, mt, yt] = dateTo.split('.');
+        const date = Date.parse(`${y}-${m}-${d}`);
+        const dateto = Date.parse(`${yt}-${mt}-${dt}`);
+        return dateto > date;
+      });
+    }
+    if (status.length !== 0) {
+      arr = arr.filter((item) => status.includes(item.status));
+    }
+    if (priceFrom !== '') {
+      arr = arr.filter(
+        (item) => parseInt(String(item.sum).replace(/\s+/g, ''), 10) > priceFrom
+      );
+    }
+    if (priceTo !== '') {
+      arr = arr.filter(
+        (item) => parseInt(String(item.sum).replace(/\s+/g, ''), 10) < priceTo
+      );
+    }
+    return arr;
   }
-  if (dateFrom !== '') {
-    arr = arr.filter((item) => {
-      const [d, m, y] = item.date.slice(0, 10).split('.');
-      const [df, mf, yf] = dateFrom.split('.');
-      const date = Date.parse(`${y}-${m}-${d}`);
-      const datefrom = Date.parse(`${yf}-${mf}-${df}`);
-      return datefrom < date;
-    });
-  }
-  if (dateTo !== '') {
-    arr = arr.filter((item) => {
-      const [d, m, y] = item.date.slice(0, 10).split('.');
-      const [dt, mt, yt] = dateTo.split('.');
-      const date = Date.parse(`${y}-${m}-${d}`);
-      const dateto = Date.parse(`${yt}-${mt}-${dt}`);
-      return dateto > date;
-    });
-  }
-  if (status.length !== 0) {
-    arr = arr.filter((item) => status.includes(item.status));
-  }
-  if (priceFrom !== '') {
-    arr = arr.filter(
-      (item) => parseInt(String(item.sum).replace(/\s+/g, ''), 10) > priceFrom
-    );
-  }
-  if (priceTo !== '') {
-    arr = arr.filter(
-      (item) => parseInt(String(item.sum).replace(/\s+/g, ''), 10) < priceTo
-    );
-  }
-  return arr;
-};
+);
