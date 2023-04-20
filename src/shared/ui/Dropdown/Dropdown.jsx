cf51a69e2970dfea2cda1ccd8525fba5bb72453a
AIllarionov <hellomyguest@yandex.ru>
2023-04-20 14:52:31 +0300

    dropdown fixed

--- src/shared/ui/Dropdown/Dropdown.jsx
+++ src/shared/ui/Dropdown/Dropdown.jsx
@@ -17,7 +17,6 @@ export function Dropdown({
   useEffect(() => {
     const handleClick = (e) => {
       if ((!isOpen && !isOpened) || containerRef.current.contains(e.target)) {
-        console.log('aa', containerRef.current.contains(e.target));
         return;
       }
       if (setOpen) {
